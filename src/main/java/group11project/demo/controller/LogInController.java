0c4b265b8046d6ae4286ff654a2b10cce22c5481
Yuhang Chen <yuhang_chen@uiowa.edu>
2019-10-28 02:57:04 -0500

    Initial commit

---
+++ src/main/java/group11project/demo/controller/LogInController.java
@@ -0,0 +1,29 @@
+package group11project.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.Map;
+
+@Controller
+public class LogInController {
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @PostMapping("/log")
+    public String Log_in(@RequestParam("username") String username,
+                         @RequestParam("password") String password){
+        if ("user".equals(username) && "password".equals(password)){
+            return "index";
+        }else {
+            return "login";
+        }
+    }
+}
+
+
