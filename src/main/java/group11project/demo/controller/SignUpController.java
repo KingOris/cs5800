ba5a005e21ebdd0e34851c24f9121173f2698926
Yuhang Chen <yuhang_chen@uiowa.edu>
2019-11-03 22:17:08 -0600

    use jdbc to check user login

--- src/main/java/group11project/demo/controller/SignUpController.java
+++ src/main/java/group11project/demo/controller/SignUpController.java
@@ -3,6 +3,7 @@ package group11project.demo.controller;
 import group11project.demo.entity.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,34 +13,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Controller
 public class SignUpController {
     @RequestMapping("/signUp")
-    public String login(){
+    public String signUp(){
         return "signUp";
     }
 
     @Resource
     private JdbcTemplate jdbcTemplate;
 
-    @PostMapping("/log")
+    @PostMapping("/signup")
     public String Log_in(@RequestParam("username") String username,
                          @RequestParam("password") String password){
-        String sql = "Select*FROM user Where id = '" + username + "'";
-        List<User> userList = jdbcTemplate.query(sql, new RowMapper<User>() {
-            User user = null;
-            @Override
-            public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                user = new User();
-                user.setId(resultSet.getString("id"));
-                user.setPassword(resultSet.getString("password"));
-                return user;
-            }
-        });
-        if (userList.get(0).getId().equals(username) && userList.get(0).getPassword().equals(password)){
-            return "index";
-        }else {
-            return "login";
-        }
-    }
-
+        String sql = "INSERT INTO user(id,password)\n VALUES (" + username +","+ password+ ")";
+        jdbcTemplate.update(sql);
+        return "index";
+}
 }
