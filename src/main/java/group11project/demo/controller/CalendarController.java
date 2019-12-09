5847a4a03b44321f86c2cc8d3d6ef9bfe49a91cc
yuhanj2 <jyhan043@gmail.com>
2019-12-08 20:51:37 -0600

    implemented CalendarController

--- src/main/java/group11project/demo/controller/CalendarController.java
+++ src/main/java/group11project/demo/controller/CalendarController.java
@@ -1,34 +1,34 @@
 package group11project.demo.controller;
 
-import group11project.demo.entity.CalendarDate;
+import group11project.demo.entity.CalendarEvent;
 import group11project.demo.entity.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-// TODO controller for calendar page
 public class CalendarController {
     @Resource
     private JdbcTemplate jdbcTemplate;
 
-    // todo request mapping
+    @RequestMapping("/calendar")
     public String getCalendarInfo(ModelMap map){
-        String sql = "Select*FROM calendar";
-        List<CalendarDate> calendar = jdbcTemplate.query(sql, new RowMapper<CalendarDate>() {
-            CalendarDate calendarDate = null;
+        String sql = "Select*FROM calendarevent";
+        List<CalendarEvent> calendar = jdbcTemplate.query(sql, new RowMapper<CalendarEvent>() {
+            CalendarEvent calendarEvent = null;
             @Override
-            public CalendarDate mapRow(ResultSet resultSet, int i) throws SQLException {
-                calendarDate = new CalendarDate();
-
-                // todo figure out how to store the event and date in mySQL
-                //calendarDate.setCalendarEvent(resultSet.getString("calendarEvent"));
-                calendarDate.setDate(resultSet.getDate("date"));
-                return calendarDate;
+            public CalendarEvent mapRow(ResultSet resultSet, int i) throws SQLException {
+                calendarEvent = new CalendarEvent();
+                calendarEvent.setDate(resultSet.getInt("date"));
+                calendarEvent.setMonth(resultSet.getInt("month"));
+                calendarEvent.setEventName(resultSet.getString("eventName"));
+                calendarEvent.setEventID(resultSet.getInt("eventID"));
+                return calendarEvent;
             }
         });
         map.addAttribute("calendar", calendar);
